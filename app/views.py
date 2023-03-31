3e88906d9e11124085dec9551ff890e80b69be8d
Mrunal <lanurm123@gmail.com>
2023-03-31 21:09:32 +0530

    commit

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
-    messages.info(request, "Welcome to CRM")
     # Check to see logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -12,7 +12,7 @@ def home(request):
         user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
-            messages.success(request,"You Have Been Logged Successfully")
+            messages.success(request,"Welcome! You Have Been Logged Successfully")
             return redirect ('home')
         else:
             messages.warning(request, "There Was an Error. Please Try Again")
@@ -29,6 +29,25 @@ def logout_user(request):
     messages.success(request,"You have Been Logged out....")
     return redirect ('home')
 
-def register_user(request):
-    return render(request,"register.html")
 
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
+
+
+        
